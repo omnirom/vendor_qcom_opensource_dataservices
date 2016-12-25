CyanogenMod/android_vendor_qcom_opensource_dataservices

dataservices: Don't pollute the tree
 * Nexus devices have a copy of this in their device repo already.
   Exclude it unless we're doing a QC-style build.

Change-Id: I2a4d3d9656be28b614e3ee59514b54801c833c3e

Android.mk
@@ -1,1 +1,3 @@
+ifeq ($(TARGET_USES_QCOM_HARDWARE),true)
 include $(call all-subdir-makefiles)
+endif

omnirom/vendor_qcom_opensource_dataservices

Android.mk
@@ -1,1 +1,3 @@
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include $(call all-subdir-makefiles)
+endif